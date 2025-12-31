2a139778d6fa02a1eca0db4df4cab229a0d01ed2
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2025-12-31 13:40:15 +0530

    Implement solution for latest day to cross problem

---
+++ LeetCode/Leetcodingchallennge 2025/December 2025/Last Day Where You Can Still Cross.py
@@ -0,0 +1,54 @@
+from collections import deque
+class Solution(object):
+    def latestDayToCross(self, row, col, cells):
+        """
+        :type row: int
+        :type col: int
+        :type cells: List[List[int]]
+        :rtype: int
+        """
+        def canCross(day):
+            grid = [[0] * col for _ in range(row)]
+            
+            # Flood cells for given day
+            for i in range(day):
+                r, c = cells[i]
+                grid[r-1][c-1] = 1
+            
+            q = deque()
+            visited = [[False] * col for _ in range(row)]
+           
+            for c in range(col):
+                if grid[0][c] == 0:
+                    q.append((0, c))
+                    visited[0][c] = True
+            
+            directions = [(1,0), (-1,0), (0,1), (0,-1)]
+            
+            while q:
+                r, c = q.popleft()
+                if r == row - 1:
+                    return True
+                
+                for dr, dc in directions:
+                    nr, nc = r + dr, c + dc
+                    if 0 <= nr < row and 0 <= nc < col:
+                        if not visited[nr][nc] and grid[nr][nc] == 0:
+                            visited[nr][nc] = True
+                            q.append((nr, nc))
+            
+            return False
+        
+      
+        left, right = 0, row * col
+        ans = 0
+        
+        while left <= right:
+            mid = (left + right) // 2
+            if canCross(mid):
+                ans = mid
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return ans
