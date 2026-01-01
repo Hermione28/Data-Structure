f034358804d9fad53f94f90d96dc4a4fa6cb4456
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-01-01 12:34:22 +0530

    Add intersectPoint method for linked list intersection
    Implement intersectPoint method to find intersection of two linked lists.

--- GeeksForGeeks/Problem Of The Solution/POTD 2026/January 2026/Day 1: Intersection in Y Shaped Lists.py
+++ GeeksForGeeks/Problem Of The Solution/POTD 2026/January 2026/Day 1: Intersection in Y Shaped Lists.py
@@ -1,1 +1,17 @@
+'''
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
+'''
+
+class Solution:
+    def intersectPoint(self, head1, head2):
+        p1, p2 = head1, head2
+        
+        while p1 != p2:
+            p1 = p1.next if p1 else head2
+            p2 = p2.next if p2 else head1
+        
+        return p1  
