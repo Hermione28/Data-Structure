389c185bf2f1ad75496cdc1c9a92a9487b95370b
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-01-03 15:17:58 +0530

    Add flatten and merge methods for linked list
    Implement flattening of a linked list with bottom pointers.

--- GeeksForGeeks/Problem Of The Solution/POTD 2026/January 2026/Day 3: Flattening a Linked List.py
+++ GeeksForGeeks/Problem Of The Solution/POTD 2026/January 2026/Day 3: Flattening a Linked List.py
@@ -1,1 +1,35 @@
+'''
+class Node:
+    def __init__(self, d):
+        self.data=d
+        self.next=None
+        self.bottom=None
+        
+'''
+class Solution:
+    def flatten(self, root):
+        if root is None or root.next is None:
+            return root
+        
+        root.next = self.flatten(root.next)
+        
+        root = self.merge(root, root.next)
+        
+        return root
+    
+    def merge(self, a, b):
+        if a is None:
+            return b
+        if b is None:
+            return a
+        
+        if a.data <= b.data:
+            result = a
+            result.bottom = self.merge(a.bottom, b)
+        else:
+            result = b
+            result.bottom = self.merge(a, b.bottom)
+        
+        result.next = None   
+        return result
 
