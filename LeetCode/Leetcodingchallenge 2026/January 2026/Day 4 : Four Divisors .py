7327b02def50cacd1a792a6286df3764ef00e450
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-01-04 13:21:55 +0530

    Add solution for sumFourDivisors method
    Implement sumFourDivisors method to calculate the sum of numbers with exactly four divisors.

--- LeetCode/Leetcodingchallenge 2026/January 2026/Day 4 : Four Divisors .py
+++ LeetCode/Leetcodingchallenge 2026/January 2026/Day 4 : Four Divisors .py
@@ -1,1 +1,31 @@
+class Solution(object):
+    def sumFourDivisors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        import math
+        total = 0
+        
+        for x in nums:
+            div_sum = 1 + x
+            count = 2  # 1 and x
+            
+            for i in range(2, int(math.sqrt(x)) + 1):
+                if x % i == 0:
+                    j = x // i
+                    if i == j:
+                        count += 1
+                        div_sum += i
+                    else:
+                        count += 2
+                        div_sum += i + j
+                    
+                    if count > 4:
+                        break
+            
+            if count == 4:
+                total += div_sum
+        
+        return total
 
