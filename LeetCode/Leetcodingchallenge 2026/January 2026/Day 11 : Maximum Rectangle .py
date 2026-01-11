4dad88e87720fde98c06e36b5086ec55ba55bea2
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-01-11 15:28:01 +0530

    Update Day 11 : Maximum Rectangle .py

--- LeetCode/Leetcodingchallenge 2026/January 2026/Day 11 : Maximum Rectangle .py
+++ LeetCode/Leetcodingchallenge 2026/January 2026/Day 11 : Maximum Rectangle .py
@@ -1,1 +1,39 @@
-
+class Solution(object):
+    def maximalRectangle(self, matrix):
+        """
+        :type matrix: List[List[str]]
+        :rtype: int
+        """
+        if not matrix or not matrix[0]:
+            return 0
+        
+        cols = len(matrix[0])
+        heights = [0] * cols
+        max_area = 0
+        
+        for row in matrix:
+            for j in range(cols):
+                if row[j] == '1':
+                    heights[j] += 1
+                else:
+                    heights[j] = 0
+            
+            max_area = max(max_area, self.largestRectangleArea(heights))
+        
+        return max_area
+    
+    def largestRectangleArea(self, heights):
+        stack = []
+        max_area = 0
+        heights.append(0)  
+        
+        for i in range(len(heights)):
+            while stack and heights[i] < heights[stack[-1]]:
+                h = heights[stack.pop()]
+                width = i if not stack else i - stack[-1] - 1
+                max_area = max(max_area, h * width)
+            stack.append(i)
+        
+        heights.pop()  
+        return max_area
+   
