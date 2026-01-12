1733b8fd8279f5ca47cd29ef376774114fd9f0ff
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-01-12 22:37:26 +0530

    Add maxOfSubarrays function using deque
    Implemented a solution to find the maximum of each subarray of size k using a deque.

--- GeeksForGeeks/Problem Of The Solution/POTD 2026/January 2026/Day 12 : K Sized Subarray Maximum .py
+++ GeeksForGeeks/Problem Of The Solution/POTD 2026/January 2026/Day 12 : K Sized Subarray Maximum .py
@@ -1,1 +1,24 @@
+from collections import deque
+
+class Solution:
+    def maxOfSubarrays(self, arr, k):
+        dq = deque()
+        result = []
+        
+        for i in range(len(arr)):
+            # Remove elements outside the current window
+            while dq and dq[0] <= i - k:
+                dq.popleft()
+            
+            # Remove smaller elements from the back
+            while dq and arr[dq[-1]] <= arr[i]:
+                dq.pop()
+            
+            dq.append(i)
+            
+            # Add maximum for current window
+            if i >= k - 1:
+                result.append(arr[dq[0]])
+        
+        return result
 
