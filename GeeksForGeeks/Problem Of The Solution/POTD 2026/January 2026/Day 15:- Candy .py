090f9c798edb7831ce6002fa8be2fdb86699c9c4
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-01-15 22:58:06 +0530

    Add minCandy method to Solution class
    Implement minCandy method to calculate minimum candies distribution.

--- GeeksForGeeks/Problem Of The Solution/POTD 2026/January 2026/Day 15:- Candy .py
+++ GeeksForGeeks/Problem Of The Solution/POTD 2026/January 2026/Day 15:- Candy .py
@@ -1,1 +1,19 @@
-
+class Solution:
+    def minCandy(self, arr):
+        n = len(arr)
+        if n == 0:
+            return 0
+        
+        candies = [1] * n
+        
+        # Left to Right
+        for i in range(1, n):
+            if arr[i] > arr[i - 1]:
+                candies[i] = candies[i - 1] + 1
+        
+        # Right to Left
+        for i in range(n - 2, -1, -1):
+            if arr[i] > arr[i + 1]:
+                candies[i] = max(candies[i], candies[i + 1] + 1)
+        
+        return sum(candies)
