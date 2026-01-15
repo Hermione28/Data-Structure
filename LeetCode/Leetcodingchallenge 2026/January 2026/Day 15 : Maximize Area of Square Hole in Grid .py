dbc5f547b9fe14c1a930bebead1bf23322201e0f
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-01-15 22:54:07 +0530

    Update Day 15 : Maximize Area of Square Hole in Grid .py

--- LeetCode/Leetcodingchallenge 2026/January 2026/Day 15 : Maximize Area of Square Hole in Grid .py
+++ LeetCode/Leetcodingchallenge 2026/January 2026/Day 15 : Maximize Area of Square Hole in Grid .py
@@ -1,1 +1,26 @@
+class Solution(object):
+    def maximizeSquareHoleArea(self, n, m, hBars, vBars):
+        """
+        :type n: int
+        :type m: int
+        :type hBars: List[int]
+        :type vBars: List[int]
+        :rtype: int
+        """
 
+        def longest_consecutive(arr):
+            arr.sort()
+            longest = curr = 1
+            for i in range(1, len(arr)):
+                if arr[i] == arr[i - 1] + 1:
+                    curr += 1
+                else:
+                    curr = 1
+                longest = max(longest, curr)
+            return longest
+        
+        max_h = longest_consecutive(hBars) + 1
+        max_v = longest_consecutive(vBars) + 1
+        
+        side = min(max_h, max_v)
+        return side * side
