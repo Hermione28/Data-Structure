0f5e1f1e4580a40c1820bf98a14a2216dab98bf6
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-01-16 22:28:15 +0530

    Update Day 16 : Maximum Square Area by Removing Fences From a Field .py

--- LeetCode/Leetcodingchallenge 2026/January 2026/Day 16 : Maximum Square Area by Removing Fences From a Field .py
+++ LeetCode/Leetcodingchallenge 2026/January 2026/Day 16 : Maximum Square Area by Removing Fences From a Field .py
@@ -1,1 +1,31 @@
-
+class Solution(object):
+    def maximizeSquareArea(self, m, n, hFences, vFences):
+        """
+        :type m: int
+        :type n: int
+        :type hFences: List[int]
+        :type vFences: List[int]
+        :rtype: int
+        """
+        MOD = 10**9 + 7
+        
+        hFences = sorted(hFences + [1, m])
+        vFences = sorted(vFences + [1, n])
+        
+        h_dist = set()
+        for i in range(len(hFences)):
+            for j in range(i + 1, len(hFences)):
+                h_dist.add(hFences[j] - hFences[i])
+        
+       
+        v_dist = set()
+        for i in range(len(vFences)):
+            for j in range(i + 1, len(vFences)):
+                v_dist.add(vFences[j] - vFences[i])
+        
+        common = h_dist & v_dist
+        if not common:
+            return -1
+        
+        side = max(common)
+        return (side * side) % MOD
