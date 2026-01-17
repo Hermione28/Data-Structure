406f8475f0f500efea957d10a33f9db988f11a66
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-01-17 23:01:50 +0530

    Update Day 17 : Expression contains redundant bracket or not .py

--- GeeksForGeeks/Problem Of The Solution/POTD 2026/January 2026/Day 17 : Expression contains redundant bracket or not .py
+++ GeeksForGeeks/Problem Of The Solution/POTD 2026/January 2026/Day 17 : Expression contains redundant bracket or not .py
@@ -1,1 +1,23 @@
+class Solution():
+    def checkRedundancy(self, s):
+        stack = []
+        operators = set(['+', '-', '*', '/'])
+        
+        for ch in s:
+            if ch == ')':
+                has_operator = False
+                
+                while stack and stack[-1] != '(':
+                    if stack[-1] in operators:
+                        has_operator = True
+                    stack.pop()
+                
+                stack.pop()
+                
+                if not has_operator:
+                    return True
+            else:
+                stack.append(ch)
+        
+        return False
 
