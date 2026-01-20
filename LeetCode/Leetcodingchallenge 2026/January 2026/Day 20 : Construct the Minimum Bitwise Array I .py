98a03aa66af9a432a3a16caeecd8315cac13b247
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-01-20 21:39:27 +0530

    Update Day 20 : Construct the Minimum Bitwise Array I .py

--- LeetCode/Leetcodingchallenge 2026/January 2026/Day 20 : Construct the Minimum Bitwise Array I .py
+++ LeetCode/Leetcodingchallenge 2026/January 2026/Day 20 : Construct the Minimum Bitwise Array I .py
@@ -1,1 +1,17 @@
+class Solution(object):
+    def minBitwiseArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        ans = []
 
+        for p in nums:
+            found = -1
+            for x in range(p + 1):
+                if (x | (x + 1)) == p:
+                    found = x
+                    break
+            ans.append(found)
+
+        return ans
