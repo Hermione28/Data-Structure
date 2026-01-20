cefa98900693b5a98c892fa9a65af7e0277b7ae5
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-01-20 21:37:18 +0530

    Add UNDO and REDO functionality to document class
    Implement methods for appending, undoing, and redoing changes in a document.

--- GeeksForGeeks/Problem Of The Solution/POTD 2026/January 2026/Day 20 : Implement UNDO & REDO .py
+++ GeeksForGeeks/Problem Of The Solution/POTD 2026/January 2026/Day 20 : Implement UNDO & REDO .py
@@ -1,1 +1,24 @@
 
+class Solution:
+    def __init__(self):
+        self.document = []      
+        self.redo_stack = []    
+
+    def append(self, x):
+        # append x into document
+        self.document.append(x)
+        self.redo_stack.clear()   
+
+    def undo(self):
+        # undo last change
+        if self.document:
+            self.redo_stack.append(self.document.pop())
+
+    def redo(self):
+        # redo changes
+        if self.redo_stack:
+            self.document.append(self.redo_stack.pop())
+
+    def read(self):
+        # read the document
+        return "".join(self.document)
