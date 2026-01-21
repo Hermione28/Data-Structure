69a293458b1aeaddd3f19cddeecf4b42ca77119c
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-01-21 23:17:51 +0530

    Update Day 21: Stock span problem .py

--- GeeksForGeeks/Problem Of The Solution/POTD 2026/January 2026/Day 21: Stock span problem .py
+++ GeeksForGeeks/Problem Of The Solution/POTD 2026/January 2026/Day 21: Stock span problem .py
@@ -1,1 +1,24 @@
+class Solution:
+    def calculateSpan(self, arr):
+        n = len(arr)
+        span = [0] * n
+        stack = []  # stack to store indices
 
+        for i in range(n):
+            # Pop elements from stack while current price is higher or equal
+            while stack and arr[stack[-1]] <= arr[i]:
+                stack.pop()
+
+            # If stack is empty, span is i+1
+            if not stack:
+                span[i] = i + 1
+            else:
+                span[i] = i - stack[-1]
+
+            # Push current index to stack
+            stack.append(i)
+
+        return span
+
+        
+        
