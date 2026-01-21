9f972e9cd8b84947c7ae32c4404352b8730206ac
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-01-21 23:21:29 +0530

    Update Day 21: Construct the Minimum Bitwise Array II .py

--- LeetCode/Leetcodingchallenge 2026/January 2026/Day 21: Construct the Minimum Bitwise Array II .py
+++ LeetCode/Leetcodingchallenge 2026/January 2026/Day 21: Construct the Minimum Bitwise Array II .py
@@ -1,1 +1,25 @@
 
+
+class Solution(object):
+    def minBitwiseArray(self, nums):
+
+        ans = []
+
+        for p in nums:
+            # Even prime (only 2)
+            if p % 2 == 0:
+                ans.append(-1)
+                continue
+
+            # Count trailing 1s
+            k = 0
+            temp = p
+            while temp & 1:
+                k += 1
+                temp >>= 1
+
+            # Correct formula
+            x = p - (1 << (k - 1))
+            ans.append(x)
+
+        return ans
