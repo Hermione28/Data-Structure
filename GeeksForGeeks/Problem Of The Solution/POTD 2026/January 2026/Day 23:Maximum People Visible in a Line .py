e7741b27988a42c43de137a36ad3b755172c145f
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-01-23 23:40:06 +0530

    Update Day 23:Maximum People Visible in a Line .py

--- GeeksForGeeks/Problem Of The Solution/POTD 2026/January 2026/Day 23:Maximum People Visible in a Line .py
+++ GeeksForGeeks/Problem Of The Solution/POTD 2026/January 2026/Day 23:Maximum People Visible in a Line .py
@@ -1,1 +1,32 @@
+class Solution:
+    def maxPeople(self, arr):
+        n = len(arr)
+        prevGE = [-1] * n
+        nextGE = [n] * n
+        stack = []
 
+        # Previous Greater or Equal
+        for i in range(n):
+            while stack and arr[stack[-1]] < arr[i]:
+                stack.pop()
+            prevGE[i] = stack[-1] if stack else -1
+            stack.append(i)
+
+        stack.clear()
+
+        # Next Greater or Equal
+        for i in range(n - 1, -1, -1):
+            while stack and arr[stack[-1]] < arr[i]:
+                stack.pop()
+            nextGE[i] = stack[-1] if stack else n
+            stack.append(i)
+
+        ans = 0
+        for i in range(n):
+            left = i - prevGE[i] - 1
+            right = nextGE[i] - i - 1
+            ans = max(ans, left + right + 1)
+
+        return ans
+
+        
