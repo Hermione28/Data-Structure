a24100b5aa1a02bf6ccfab974be2e072dd030afc
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-01-24 17:20:15 +0530

    Implement minPairSum to minimize maximum pair sum

--- LeetCode/Leetcodingchallenge 2026/January 2026/Day 24 : Minimize Maximum Pair Sum in Array .py
+++ LeetCode/Leetcodingchallenge 2026/January 2026/Day 24 : Minimize Maximum Pair Sum in Array .py
@@ -1,1 +1,12 @@
-
+class Solution(object):
+    def minPairSum(self, nums):
+        nums.sort()
+        i, j = 0, len(nums) - 1
+        max_sum = 0
+        
+        while i < j:
+            max_sum = max(max_sum, nums[i] + nums[j])
+            i += 1
+            j -= 1
+        
+        return max_sum
