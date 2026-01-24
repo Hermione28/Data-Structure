57581f198b5bb6b0385093faa8bedb84ef06b345
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-01-24 19:42:31 +0530

    Update Day 24 : Josephus problem .py

--- GeeksForGeeks/Problem Of The Solution/POTD 2026/January 2026/Day 24 : Josephus problem .py
+++ GeeksForGeeks/Problem Of The Solution/POTD 2026/January 2026/Day 24 : Josephus problem .py
@@ -1,1 +1,7 @@
+class Solution:
+    def josephus(self, n, k):
+        res = 0  # f(1, k) = 0 (0-based index)
+        for i in range(2, n + 1):
+            res = (res + k) % i
+        return res + 1  # convert to 1-based index
 
