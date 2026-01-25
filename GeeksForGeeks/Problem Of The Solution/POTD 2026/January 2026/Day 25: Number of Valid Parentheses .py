59d5cde8644b19ba24fda078e328e57d60c58588
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-01-25 21:26:24 +0530

    Update Day 25: Number of Valid Parentheses .py

--- GeeksForGeeks/Problem Of The Solution/POTD 2026/January 2026/Day 25: Number of Valid Parentheses .py
+++ GeeksForGeeks/Problem Of The Solution/POTD 2026/January 2026/Day 25: Number of Valid Parentheses .py
@@ -1,1 +1,19 @@
+class Solution:
+    def findWays(self, n):
+        # If length is odd, no valid parentheses possible
+        if n % 2 != 0:
+            return 0
+        
+        pairs = n // 2
+        
+        # DP array for Catalan numbers
+        dp = [0] * (pairs + 1)
+        dp[0] = 1
+        
+        for i in range(1, pairs + 1):
+            for j in range(i):
+                dp[i] += dp[j] * dp[i - 1 - j]
+        
+        return dp[pairs]
 
+        
