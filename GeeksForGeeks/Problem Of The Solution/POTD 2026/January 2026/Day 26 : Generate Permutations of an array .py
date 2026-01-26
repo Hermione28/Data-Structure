68ad31c4bebc6680b6efbd43fbfdf60cd7e31439
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-01-26 22:14:46 +0530

    Update Day 26 : Generate Permutations of an array .py

--- GeeksForGeeks/Problem Of The Solution/POTD 2026/January 2026/Day 26 : Generate Permutations of an array .py
+++ GeeksForGeeks/Problem Of The Solution/POTD 2026/January 2026/Day 26 : Generate Permutations of an array .py
@@ -1,1 +1,26 @@
+class Solution:
+    def permuteDist(self, arr):
+        result = []
+        used = [False] * len(arr)
+        
+        def backtrack(path):
+            if len(path) == len(arr):
+                result.append(path[:])
+                return
+            
+            for i in range(len(arr)):
+                if used[i]:
+                    continue
+                used[i] = True
+                path.append(arr[i])
+                backtrack(path)
+                path.pop()
+                used[i] = False
+        
+        backtrack([])
+        return result
 
+
+        
+        
+        
