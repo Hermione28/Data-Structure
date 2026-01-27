e89b8d91a9204f547f0493e88e4d091481ba54db
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-01-27 18:11:39 +0530

    Add word search algorithm using DFS
    Implement depth-first search to check for word existence in a matrix.

--- GeeksForGeeks/Problem Of The Solution/POTD 2026/January 2026/Day 27 : Word Search .py
+++ GeeksForGeeks/Problem Of The Solution/POTD 2026/January 2026/Day 27 : Word Search .py
@@ -1,1 +1,36 @@
+class Solution:
+    def isWordExist(self, mat, word):
+        n, m = len(mat), len(mat[0])
+        
+        def dfs(i, j, idx):
+            # If full word is matched
+            if idx == len(word):
+                return True
+            
+            # Boundary and mismatch checks
+            if i < 0 or i >= n or j < 0 or j >= m or mat[i][j] != word[idx]:
+                return False
+            
+            # Mark cell as visited
+            temp = mat[i][j]
+            mat[i][j] = '#'
+            
+            # Explore all 4 directions
+            found = (dfs(i+1, j, idx+1) or
+                     dfs(i-1, j, idx+1) or
+                     dfs(i, j+1, idx+1) or
+                     dfs(i, j-1, idx+1))
+            
+            # Backtrack
+            mat[i][j] = temp
+            return found
+        
+        # Try starting DFS from every cell
+        for i in range(n):
+            for j in range(m):
+                if mat[i][j] == word[0]:
+                    if dfs(i, j, 0):
+                        return True
+        
+        return False
 
