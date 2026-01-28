6cd9b7350c2bf603b7ab6325da316ed6de135320
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-01-28 22:10:20 +0530

    Update Day 28 : Count Subset With Target Sum II .py

--- GeeksForGeeks/Problem Of The Solution/POTD 2026/January 2026/Day 28 : Count Subset With Target Sum II .py
+++ GeeksForGeeks/Problem Of The Solution/POTD 2026/January 2026/Day 28 : Count Subset With Target Sum II .py
@@ -1,1 +1,28 @@
+from collections import defaultdict
 
+class Solution:
+    def countSubset(self, arr, k):
+        n = len(arr)
+        mid = n // 2
+        
+        left = arr[:mid]
+        right = arr[mid:]
+        
+        def subset_sums(nums):
+            sums = [0]
+            for num in nums:
+                sums += [num + s for s in sums]
+            return sums
+        
+        left_sums = subset_sums(left)
+        right_sums = subset_sums(right)
+        
+        freq = defaultdict(int)
+        for s in right_sums:
+            freq[s] += 1
+        
+        count = 0
+        for s in left_sums:
+            count += freq[k - s]
+        
+        return count
