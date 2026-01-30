6aa47e700c22a2425dc035d9e5fcd3d2040e790b
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-01-30 22:16:46 +0530

    Update Day 30 : Interleave the First Half of the Queue with Second Half .py

--- GeeksForGeeks/Problem Of The Solution/POTD 2026/January 2026/Day 30 : Interleave the First Half of the Queue with Second Half .py
+++ GeeksForGeeks/Problem Of The Solution/POTD 2026/January 2026/Day 30 : Interleave the First Half of the Queue with Second Half .py
@@ -1,1 +1,17 @@
+from collections import deque
+
+class Solution:
+    def rearrangeQueue(self, q):
+        n = len(q)
+        half = n // 2
+        
+        first_half = deque()
+        
+        for _ in range(half):
+            first_half.append(q.popleft())
+        
+        while first_half:
+            q.append(first_half.popleft())
+            q.append(q.popleft())
+
 
