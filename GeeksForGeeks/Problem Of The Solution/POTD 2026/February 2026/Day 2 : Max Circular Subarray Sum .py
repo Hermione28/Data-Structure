11f55d0bfae1895fd6f2a05603568ca90edc8b66
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-02-02 17:34:18 +0530

    Add maxCircularSum method to Solution class
    Implement maxCircularSum method to calculate maximum circular subarray sum.

--- GeeksForGeeks/Problem Of The Solution/POTD 2026/February 2026/Day 2 : Max Circular Subarray Sum .py
+++ GeeksForGeeks/Problem Of The Solution/POTD 2026/February 2026/Day 2 : Max Circular Subarray Sum .py
@@ -1,1 +1,25 @@
+class Solution:
+    def maxCircularSum(self, arr):
+        total_sum = 0
+        
+        max_ending = min_ending = 0
+        max_sum = -10**18
+        min_sum = 10**18
+        
+        for x in arr:
+            
+            max_ending = max(x, max_ending + x)
+            max_sum = max(max_sum, max_ending)
+            
+            
+            min_ending = min(x, min_ending + x)
+            min_sum = min(min_sum, min_ending)
+            
+            total_sum += x
+        
+       
+        if max_sum < 0:
+            return max_sum
+        
+        return max(max_sum, total_sum - min_sum)
 
