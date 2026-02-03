a686e0d05751357f121e33463bcde9eb1af78c58
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-02-03 21:52:11 +0530

    Update Day 3 : Trionic Array I .py

--- LeetCode/Leetcodingchallenge 2026/February 2026/Day 3 : Trionic Array I .py
+++ LeetCode/Leetcodingchallenge 2026/February 2026/Day 3 : Trionic Array I .py
@@ -1,1 +1,21 @@
-
+class Solution(object):
+    def isTrionic(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        n = len(nums)
+        # p: end index of first increasing segment (0..p)
+        for p in range(1, n-2):  # need at least one element after decreasing segment
+            for q in range(p+1, n-1):  # need at least one element for third segment
+                # check first segment strictly increasing
+                if not all(nums[i] < nums[i+1] for i in range(p)):
+                    continue
+                # check second segment strictly decreasing
+                if not all(nums[i] > nums[i+1] for i in range(p, q)):
+                    continue
+                # check third segment strictly increasing
+                if not all(nums[i] < nums[i+1] for i in range(q, n-1)):
+                    continue
+                return True
+        return False
