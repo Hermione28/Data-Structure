0bb2565c3c66231e4bf7531263ddc3ba222d5e78
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-02-07 15:49:09 +0530

    Update Day 7 : Max sum in the configuration .py

--- GeeksForGeeks/Problem Of The Solution/POTD 2026/February 2026/Day 7 : Max sum in the configuration .py
+++ GeeksForGeeks/Problem Of The Solution/POTD 2026/February 2026/Day 7 : Max sum in the configuration .py
@@ -1,1 +1,21 @@
+class Solution:
+    def maxSum(self, arr):
+        n = len(arr)
+        
+        # Sum of array elements
+        arrSum = sum(arr)
+        
+        # Initial value of i*arr[i]
+        currVal = 0
+        for i in range(n):
+            currVal += i * arr[i]
+        
+        maxVal = currVal
+        
+        # Compute subsequent values using rotation formula
+        for i in range(1, n):
+            currVal = currVal + arrSum - n * arr[n - i]
+            maxVal = max(maxVal, currVal)
+        
+        return maxVal
 
