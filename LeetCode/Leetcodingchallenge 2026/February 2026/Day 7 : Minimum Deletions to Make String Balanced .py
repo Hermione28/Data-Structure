bee6db21b909e6d43df0741dee8d7c06af555a16
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-02-07 15:48:47 +0530

    Update Day 7 : Minimum Deletions to Make String Balanced .py

--- LeetCode/Leetcodingchallenge 2026/February 2026/Day 7 : Minimum Deletions to Make String Balanced .py
+++ LeetCode/Leetcodingchallenge 2026/February 2026/Day 7 : Minimum Deletions to Make String Balanced .py
@@ -1,1 +1,18 @@
+class Solution(object):
+    def minimumDeletions(self, s):
+        a_right = s.count('a')   # total 'a's
+        b_left = 0
+        
+        # IMPORTANT: consider split before index 0
+        ans = a_right
+        
+        for ch in s:
+            if ch == 'a':
+                a_right -= 1
+            else:  # ch == 'b'
+                b_left += 1
+            
+            ans = min(ans, b_left + a_right)
+        
+        return ans
 
