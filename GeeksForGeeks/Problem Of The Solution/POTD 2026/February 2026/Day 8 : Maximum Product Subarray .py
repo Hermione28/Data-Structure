6a7cbd4fec92e9fa4b5e1c47ef73f6d603dd57a0
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-02-09 22:23:42 +0530

    Update Day 8 : Maximum Product Subarray .py

--- GeeksForGeeks/Problem Of The Solution/POTD 2026/February 2026/Day 8 : Maximum Product Subarray .py
+++ GeeksForGeeks/Problem Of The Solution/POTD 2026/February 2026/Day 8 : Maximum Product Subarray .py
@@ -1,1 +1,21 @@
+class Solution:
+    def maxProduct(self, arr):
+        # Initialize variables
+        max_ending = arr[0]
+        min_ending = arr[0]
+        result = arr[0]
+
+        for i in range(1, len(arr)):
+            # If current element is negative, swap
+            if arr[i] < 0:
+                max_ending, min_ending = min_ending, max_ending
+
+            # Update max and min ending here
+            max_ending = max(arr[i], max_ending * arr[i])
+            min_ending = min(arr[i], min_ending * arr[i])
+
+            # Update result
+            result = max(result, max_ending)
+
+        return result
 
