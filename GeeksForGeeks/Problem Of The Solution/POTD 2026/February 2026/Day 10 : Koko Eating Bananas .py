082f475805c2d0b47c1cb1252c3d2301d1c64b17
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-02-12 19:21:12 +0530

    Add Koko Eating Bananas solution
    Implement binary search to determine minimum eating speed for Koko.

--- GeeksForGeeks/Problem Of The Solution/POTD 2026/February 2026/Day 10 : Koko Eating Bananas .py
+++ GeeksForGeeks/Problem Of The Solution/POTD 2026/February 2026/Day 10 : Koko Eating Bananas .py
@@ -1,1 +1,23 @@
+class Solution:
+    def kokoEat(self, arr, k):
+        import math
+        
+        low, high = 1, max(arr)
+        ans = high
+        
+        while low <= high:
+            mid = (low + high) // 2
+            
+            hours = 0
+            for bananas in arr:
+                hours += math.ceil(bananas / mid)
+            
+            if hours <= k:
+                ans = mid
+                high = mid - 1
+            else:
+                low = mid + 1
+        
+        return ans
 
+        
