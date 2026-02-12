272d9be0aba6f0477585c497459ae236e72963b2
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-02-12 20:44:55 +0530

    Update Day 12 : Max min Height .py

--- GeeksForGeeks/Problem Of The Solution/POTD 2026/February 2026/Day 12 : Max min Height .py
+++ GeeksForGeeks/Problem Of The Solution/POTD 2026/February 2026/Day 12 : Max min Height .py
@@ -1,1 +1,42 @@
+class Solution():
+    def maxMinHeight(self, arr, k, w):
+        n = len(arr)
+        
+        def canAchieve(target):
+            temp = [0] * (n + 1)
+            curr_add = 0
+            operations = 0
+            
+            for i in range(n):
+                curr_add += temp[i]
+                current_height = arr[i] + curr_add
+                
+                if current_height < target:
+                    need = target - current_height
+                    operations += need
+                    
+                    if operations > k:
+                        return False
+                    
+                    curr_add += need
+                    if i + w < n:
+                        temp[i + w] -= need
+            
+            return True
+        
+        low = min(arr)
+        high = min(arr) + k
+        ans = low
+        
+        while low <= high:
+            mid = (low + high) // 2
+            
+            if canAchieve(mid):
+                ans = mid
+                low = mid + 1
+            else:
+                high = mid - 1
+                
+        return ans
 
+        
