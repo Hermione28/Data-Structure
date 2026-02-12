563ca30696a0e0c5556adccf64204d9549b563e2
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-02-12 22:32:36 +0530

    Update Day 12 : Longest Balanced Substring I .py

--- LeetCode/Leetcodingchallenge 2026/February 2026/Day 12 : Longest Balanced Substring I .py
+++ LeetCode/Leetcodingchallenge 2026/February 2026/Day 12 : Longest Balanced Substring I .py
@@ -1,1 +1,27 @@
 
+class Solution(object):
+    def longestBalanced(self, s):
+        n = len(s)
+        max_length = 0
+        
+        for i in range(n):
+            freq = [0] * 26
+            maxFreq = 0
+            distinct = 0
+            
+            for j in range(i, n):
+                idx = ord(s[j]) - ord('a')
+                
+                if freq[idx] == 0:
+                    distinct += 1
+                    
+                freq[idx] += 1
+                maxFreq = max(maxFreq, freq[idx])
+                
+                length = j - i + 1
+                
+                # Check balanced condition
+                if length == distinct * maxFreq:
+                    max_length = max(max_length, length)
+        
+        return max_length
