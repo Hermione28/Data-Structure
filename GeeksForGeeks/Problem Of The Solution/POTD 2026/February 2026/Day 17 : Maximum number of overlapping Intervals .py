7473bff7a45dae56a187784fd885741352280733
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-02-17 19:33:33 +0530

    Update Day 17 : Maximum number of overlapping Intervals .py

--- GeeksForGeeks/Problem Of The Solution/POTD 2026/February 2026/Day 17 : Maximum number of overlapping Intervals .py
+++ GeeksForGeeks/Problem Of The Solution/POTD 2026/February 2026/Day 17 : Maximum number of overlapping Intervals .py
@@ -1,1 +1,26 @@
+class Solution:
+    def overlapInt(self, arr):
+        n = len(arr)
+        
+        # Separate start and end times
+        start = sorted([interval[0] for interval in arr])
+        end = sorted([interval[1] for interval in arr])
+        
+        i = j = 0
+        curr_overlap = 0
+        max_overlap = 0
+        
+        # Traverse both arrays
+        while i < n and j < n:
+            if start[i] <= end[j]:   # inclusive overlap
+                curr_overlap += 1
+                max_overlap = max(max_overlap, curr_overlap)
+                i += 1
+            else:
+                curr_overlap -= 1
+                j += 1
+                
+        return max_overlap
+
+        
 
