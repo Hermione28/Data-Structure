82b951958d20b5bd7bc32e502b3b439125c65681
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-02-18 21:35:16 +0530

    Add inversion count algorithm using merge sort
    Implemented a solution to count inversions in an array using merge sort.

--- GeeksForGeeks/Problem Of The Solution/POTD 2026/February 2026/Day 18 : Count Inversions .py
+++ GeeksForGeeks/Problem Of The Solution/POTD 2026/February 2026/Day 18 : Count Inversions .py
@@ -1,1 +1,33 @@
+class Solution:
+    def inversionCount(self, arr):
+        def merge_sort(nums):
+            if len(nums) <= 1:
+                return nums, 0
+            
+            mid = len(nums) // 2
+            left, inv_left = merge_sort(nums[:mid])
+            right, inv_right = merge_sort(nums[mid:])
+            
+            merged, inv_merge = merge(left, right)
+            return merged, inv_left + inv_right + inv_merge
+        
+        def merge(left, right):
+            i = j = inv_count = 0
+            merged = []
+            
+            while i < len(left) and j < len(right):
+                if left[i] <= right[j]:
+                    merged.append(left[i])
+                    i += 1
+                else:
+                    merged.append(right[j])
+                    inv_count += len(left) - i  
+                    j += 1
+            
+            merged.extend(left[i:])
+            merged.extend(right[j:])
+            return merged, inv_count
+        
+        _, count = merge_sort(arr)
+        return count
 
