423479b2eb0d80adeb996237ebb82abd10a069b0
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-02-20 20:25:19 +0530

    Implement method to find the largest number from array

--- GeeksForGeeks/Problem Of The Solution/POTD 2026/February 2026/Day 20 : Form the Largest Number .py
+++ GeeksForGeeks/Problem Of The Solution/POTD 2026/February 2026/Day 20 : Form the Largest Number .py
@@ -1,1 +1,15 @@
-
+class Solution:
+    def findLargest(self, arr):
+        # Convert integers to strings
+        arr = list(map(str, arr))
+        
+        # Custom comparator using sort key
+        arr.sort(key=lambda x: x*10, reverse=True)
+        
+        # Edge case: if highest is "0", result is "0"
+        if arr[0] == "0":
+            return "0"
+        
+        # Join all numbers
+        return ''.join(arr)
+	    
