fcac0f547716bd59a823b5963abe6821da0b6afa
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-02-22 21:43:28 +0530

    Update Day 22 : Binary Gap .py

--- LeetCode/Leetcodingchallenge 2026/February 2026/Day 22 : Binary Gap .py
+++ LeetCode/Leetcodingchallenge 2026/February 2026/Day 22 : Binary Gap .py
@@ -1,1 +1,22 @@
 
+
+class Solution(object):
+    def binaryGap(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        last_pos = -1
+        max_gap = 0
+        position = 0
+        
+        while n > 0:
+            if n & 1:  # if current bit is 1
+                if last_pos != -1:
+                    max_gap = max(max_gap, position - last_pos)
+                last_pos = position
+            
+            n >>= 1
+            position += 1
+        
+        return max_gap
