46e0978d63cccb5d812e6d6925e9486bc012aaa8
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-02-23 23:12:36 +0530

    Update Day 23 :  Check If a String Contains All Binary Codes of Size K  .py

--- LeetCode/Leetcodingchallenge 2026/February 2026/Day 23 :  Check If a String Contains All Binary Codes of Size K  .py
+++ LeetCode/Leetcodingchallenge 2026/February 2026/Day 23 :  Check If a String Contains All Binary Codes of Size K  .py
@@ -1,1 +1,27 @@
-
+class Solution(object):
+    def hasAllCodes(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: bool
+        """
+        n = len(s)
+        if k > n:
+            return False
+        
+        needed = 1 << k  
+        seen = set()
+        
+        hash_val = 0
+        mask = needed - 1
+        
+        for i in range(n):
+            hash_val = ((hash_val << 1) & mask) | int(s[i])
+            
+            
+            if i >= k - 1:
+                seen.add(hash_val)
+                if len(seen) == needed:
+                    return True
+        
+        return False
