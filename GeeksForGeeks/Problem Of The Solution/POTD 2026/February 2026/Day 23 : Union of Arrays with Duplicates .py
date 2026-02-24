f596f30262227c01b4d42f6166545b0b9d14d30a
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-02-24 20:48:27 +0530

    Update Day 23 : Union of Arrays with Duplicates .py

--- GeeksForGeeks/Problem Of The Solution/POTD 2026/February 2026/Day 23 : Union of Arrays with Duplicates .py
+++ GeeksForGeeks/Problem Of The Solution/POTD 2026/February 2026/Day 23 : Union of Arrays with Duplicates .py
@@ -1,1 +1,13 @@
-
+class Solution:    
+    def findUnion(self, a, b):
+        # Use a set to store distinct elements
+        union_set = set()
+        
+        for num in a:
+            union_set.add(num)
+        
+        for num in b:
+            union_set.add(num)
+        
+        # Return as a list (driver will sort)
+        return list(union_set)
