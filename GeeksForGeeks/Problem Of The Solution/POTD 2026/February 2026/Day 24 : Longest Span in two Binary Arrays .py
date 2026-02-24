ef1336250705d12de8d4d3eb8dc956d5c65ac819
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-02-24 20:48:49 +0530

    Update Day 24 : Longest Span in two Binary Arrays .py

--- GeeksForGeeks/Problem Of The Solution/POTD 2026/February 2026/Day 24 : Longest Span in two Binary Arrays .py
+++ GeeksForGeeks/Problem Of The Solution/POTD 2026/February 2026/Day 24 : Longest Span in two Binary Arrays .py
@@ -1,1 +1,22 @@
-
+class Solution:
+    def equalSumSpan(self, a1, a2):
+        n = len(a1)
+        diff_index = {}   # stores first occurrence of diff
+        diff = 0
+        max_len = 0
+        
+        for i in range(n):
+            diff += a1[i] - a2[i]
+            
+            # If diff is 0, span from 0 to i has equal sum
+            if diff == 0:
+                max_len = i + 1
+            
+            # If diff seen before, span between previous index+1 to i
+            if diff in diff_index:
+                max_len = max(max_len, i - diff_index[diff])
+            else:
+                diff_index[diff] = i
+        
+        return max_len
+        
