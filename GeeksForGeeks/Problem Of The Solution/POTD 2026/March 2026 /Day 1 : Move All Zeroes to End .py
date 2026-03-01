3b8a48f743f05038225bb64135b39b58d51f4fb4
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-03-01 23:02:02 +0530

    Update Day 1 : Move All Zeroes to End .py

--- GeeksForGeeks/Problem Of The Solution/POTD 2026/March 2026 /Day 1 : Move All Zeroes to End .py
+++ GeeksForGeeks/Problem Of The Solution/POTD 2026/March 2026 /Day 1 : Move All Zeroes to End .py
@@ -1,1 +1,8 @@
+class Solution:
+    def pushZerosToEnd(self, arr):
+        pos = 0  # position to place the next non-zero
 
+        for i in range(len(arr)):
+            if arr[i] != 0:
+                arr[pos], arr[i] = arr[i], arr[pos]
+                pos += 1
