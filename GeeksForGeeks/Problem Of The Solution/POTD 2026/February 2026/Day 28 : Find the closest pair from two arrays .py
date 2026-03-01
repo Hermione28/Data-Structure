b8f89cec9af5c45c2d0256a1f9cf28b2171084e0
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-03-01 19:18:31 +0530

    Add findClosestPair method to Solution class
    Implemented a method to find the closest pair from two arrays that sums up to a given value.

--- GeeksForGeeks/Problem Of The Solution/POTD 2026/February 2026/Day 28 : Find the closest pair from two arrays .py
+++ GeeksForGeeks/Problem Of The Solution/POTD 2026/February 2026/Day 28 : Find the closest pair from two arrays .py
@@ -1,1 +1,26 @@
+class Solution:
+    def findClosestPair(self, arr1, arr2, x):
+        n = len(arr1)
+        m = len(arr2)
+
+        i = 0
+        j = m - 1
+
+        closest_pair = (arr1[0], arr2[0])
+        min_diff = float('inf')
+
+        while i < n and j >= 0:
+            s = arr1[i] + arr2[j]
+            diff = abs(s - x)
+
+            if diff < min_diff:
+                min_diff = diff
+                closest_pair = (arr1[i], arr2[j])
+
+            if s > x:
+                j -= 1
+            else:
+                i += 1
+
+        return list(closest_pair)
 
