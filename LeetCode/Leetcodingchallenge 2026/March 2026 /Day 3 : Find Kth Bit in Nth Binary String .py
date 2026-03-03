a853c6cff520dd45b1d555b8f2a438e6130d0be9
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-03-03 18:34:23 +0530

    Update Day 3 : Find Kth Bit in Nth Binary String .py

--- LeetCode/Leetcodingchallenge 2026/March 2026 /Day 3 : Find Kth Bit in Nth Binary String .py
+++ LeetCode/Leetcodingchallenge 2026/March 2026 /Day 3 : Find Kth Bit in Nth Binary String .py
@@ -1,1 +1,25 @@
 
+class Solution(object):
+    def findKthBit(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: str
+        """
+        # Base case
+        if n == 1:
+            return "0"
+        
+        mid = 2 ** (n - 1)
+        
+        if k == mid:
+            return "1"
+        elif k < mid:
+            return self.findKthBit(n - 1, k)
+        else:
+            # position in reversed part
+            new_k = 2 ** n - k
+            bit = self.findKthBit(n - 1, new_k)
+            
+            # invert the bit
+            return "1" if bit == "0" else "0"
