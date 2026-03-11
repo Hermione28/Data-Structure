fe73d7fd6ae514d46b37683d450a9e5ff808f4f8
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-03-11 21:00:29 +0530

    Update Day 11 : Complement of Base 10 Integer .py

--- LeetCode/Leetcodingchallenge 2026/March 2026 /Day 11 : Complement of Base 10 Integer .py
+++ LeetCode/Leetcodingchallenge 2026/March 2026 /Day 11 : Complement of Base 10 Integer .py
@@ -1,1 +1,11 @@
-
+class Solution(object):
+    def bitwiseComplement(self, n):
+        if n == 0:
+            return 1
+        
+        mask = 1
+        while mask <= n:
+            mask <<= 1
+        
+        return (mask - 1) ^ n
+        
