d85c5ffecc678cd5b18ee77e34c98ba7a7711542
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-03-11 21:14:30 +0530

    Update Day 9 : Largest number in one swap .py

--- GeeksForGeeks/Problem Of The Solution/POTD 2026/March 2026 /Day 9 : Largest number in one swap .py
+++ GeeksForGeeks/Problem Of The Solution/POTD 2026/March 2026 /Day 9 : Largest number in one swap .py
@@ -1,1 +1,21 @@
-
+class Solution:
+    def largestSwap(self, s: str) -> str:
+        n = len(s)
+        s = list(s)  
+        
+        
+        last_pos = {int(s[i]): i for i in range(n)}
+        
+       
+        for i in range(n):
+            current = int(s[i])
+            
+            for d in range(9, current, -1):  
+                if d in last_pos and last_pos[d] > i:
+                  
+                    j = last_pos[d]
+                    s[i], s[j] = s[j], s[i]
+                    return "".join(s)
+         
+        return "".join(s)
+        
