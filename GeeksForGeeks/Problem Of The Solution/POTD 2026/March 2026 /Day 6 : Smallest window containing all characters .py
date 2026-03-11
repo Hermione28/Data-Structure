b3d0ca75b92e73c5c33f00ddd1ef1cf79e0108a4
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-03-11 21:13:21 +0530

    Add minWindow function for smallest window problem
    Implement minWindow function to find the smallest substring containing all characters of another string.

--- GeeksForGeeks/Problem Of The Solution/POTD 2026/March 2026 /Day 6 : Smallest window containing all characters .py
+++ GeeksForGeeks/Problem Of The Solution/POTD 2026/March 2026 /Day 6 : Smallest window containing all characters .py
@@ -1,1 +1,38 @@
 
+from collections import Counter
+
+class Solution:
+    def minWindow(self, s, p):
+        if len(p) > len(s):
+            return ""
+        
+        need = Counter(p)  
+        have = {}
+        
+        required = len(need)  
+        formed = 0            
+        
+        left = 0
+        min_len = float("inf")
+        start = 0
+        
+        for right, char in enumerate(s):
+            have[char] = have.get(char, 0) + 1
+            
+            if char in need and have[char] == need[char]:
+                formed += 1
+            
+            while formed == required:
+                if right - left + 1 < min_len:
+                    min_len = right - left + 1
+                    start = left
+                
+                have[s[left]] -= 1
+                if s[left] in need and have[s[left]] < need[s[left]]:
+                    formed -= 1
+                left += 1
+        
+        return "" if min_len == float("inf") else s[start:start + min_len]
+
+        # code here
+        
