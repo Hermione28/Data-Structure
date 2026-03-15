21feea25e3185399ffb050939106e5567374557a
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-03-15 13:41:13 +0530

    Update Day 14 : The k-th Lexicographical String of All Happy Strings of Length n .py

--- LeetCode/Leetcodingchallenge 2026/March 2026 /Day 14 : The k-th Lexicographical String of All Happy Strings of Length n .py
+++ LeetCode/Leetcodingchallenge 2026/March 2026 /Day 14 : The k-th Lexicographical String of All Happy Strings of Length n .py
@@ -1,1 +1,22 @@
-
+class Solution(object):
+    def getHappyString(self, n, k):
+        res = []
+        
+        def backtrack(s):
+            if len(res) >= k:
+                return
+            
+            if len(s) == n:
+                res.append(s)
+                return
+            
+            for ch in ['a','b','c']:
+                if not s or s[-1] != ch:
+                    backtrack(s + ch)
+        
+        backtrack("")
+        
+        if k <= len(res):
+            return res[k-1]
+        return ""
+        
