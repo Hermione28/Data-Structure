36282ffaa6c6f346c0e50778ce1ad6ace90d92dc
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-03-15 13:40:50 +0530

    Implement Fancy class for sequence manipulation

--- LeetCode/Leetcodingchallenge 2026/March 2026 /Day 15 : Fancy Sequence .py
+++ LeetCode/Leetcodingchallenge 2026/March 2026 /Day 15 : Fancy Sequence .py
@@ -1,1 +1,25 @@
+class Fancy(object):
 
+    def __init__(self):
+        self.MOD = 10**9 + 7
+        self.seq = []
+        self.mul = 1
+        self.add = 0
+
+    def append(self, val):
+        inv = pow(self.mul, self.MOD - 2, self.MOD)
+        val = (val - self.add) % self.MOD
+        val = (val * inv) % self.MOD
+        self.seq.append(val)
+
+    def addAll(self, inc):
+        self.add = (self.add + inc) % self.MOD
+
+    def multAll(self, m):
+        self.mul = (self.mul * m) % self.MOD
+        self.add = (self.add * m) % self.MOD
+
+    def getIndex(self, idx):
+        if idx >= len(self.seq):
+            return -1
+        return (self.seq[idx] * self.mul + self.add) % self.MOD
