18e28395607ef96f5a03f66a4fd8473837da4a3a
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-03-17 22:47:11 +0530

    Update Day 17 : Largest Submatrix With Rearrangements .py

--- LeetCode/Leetcodingchallenge 2026/March 2026 /Day 17 : Largest Submatrix With Rearrangements .py
+++ LeetCode/Leetcodingchallenge 2026/March 2026 /Day 17 : Largest Submatrix With Rearrangements .py
@@ -1,1 +1,23 @@
-
+class Solution(object):
+    def largestSubmatrix(self, matrix):
+        m, n = len(matrix), len(matrix[0])
+        
+        
+        for i in range(1, m):
+            for j in range(n):
+                if matrix[i][j] == 1:
+                    matrix[i][j] += matrix[i-1][j]
+        
+        max_area = 0
+        
+      
+        for row in matrix:
+            row.sort(reverse=True)  
+            
+            for j in range(n):
+                height = row[j]
+                width = j + 1
+                max_area = max(max_area, height * width)
+        
+        return max_area
+        
