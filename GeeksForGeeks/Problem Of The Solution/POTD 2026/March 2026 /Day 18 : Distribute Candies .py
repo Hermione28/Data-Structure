28aed947aa8df8a93335a4d5a7a99d7b0a1991b6
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-03-18 11:20:06 +0530

    Update Day 18 : Distribute Candies .py

--- GeeksForGeeks/Problem Of The Solution/POTD 2026/March 2026 /Day 18 : Distribute Candies .py
+++ GeeksForGeeks/Problem Of The Solution/POTD 2026/March 2026 /Day 18 : Distribute Candies .py
@@ -1,1 +1,27 @@
+'''
+class Node:
+    def __init__(self, val):
+        self.data = val
+        self.right = None
+        self.left = None
+'''
 
+class Solution:
+    def distCandy(self, root):
+        self.moves = 0
+        
+        def dfs(node):
+            if not node:
+                return 0
+            
+            left = dfs(node.left)
+            right = dfs(node.right)
+            
+            # Count moves
+            self.moves += abs(left) + abs(right)
+            
+            # Return balance
+            return node.data + left + right - 1
+        
+        dfs(root)
+        return self.moves
