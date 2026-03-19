1003db6fa0148cca965eaa549cb2e8a2f9100b8d
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-03-19 23:29:13 +0530

    Update Day 19 : Largest BST .py

--- GeeksForGeeks/Problem Of The Solution/POTD 2026/March 2026 /Day 19 : Largest BST .py
+++ GeeksForGeeks/Problem Of The Solution/POTD 2026/March 2026 /Day 19 : Largest BST .py
@@ -1,1 +1,29 @@
-
+class Solution:
+    def largestBst(self, root):
+        
+        self.maxSize = 0
+        
+        def solve(node):
+            # Base case
+            if not node:
+                return (True, 0, float('inf'), float('-inf'))
+            
+            left = solve(node.left)
+            right = solve(node.right)
+            
+            # Check BST condition
+            if left[0] and right[0] and left[3] < node.data < right[2]:
+                
+                size = left[1] + right[1] + 1
+                self.maxSize = max(self.maxSize, size)
+                
+                min_val = min(left[2], node.data)
+                max_val = max(right[3], node.data)
+                
+                return (True, size, min_val, max_val)
+            
+            # Not a BST
+            return (False, 0, 0, 0)
+        
+        solve(root)
+        return self.maxSize
