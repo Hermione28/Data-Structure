0204a063718a18fd0f5a8fe2b16c0b94b655773c
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-03-22 14:15:52 +0530

    Update Day 22 : Determine Whether Matrix Can Be Obtained By Rotation .py

--- LeetCode/Leetcodingchallenge 2026/March 2026 /Day 22 : Determine Whether Matrix Can Be Obtained By Rotation .py
+++ LeetCode/Leetcodingchallenge 2026/March 2026 /Day 22 : Determine Whether Matrix Can Be Obtained By Rotation .py
@@ -1,1 +1,22 @@
-
+class Solution(object):
+    def findRotation(self, mat, target):
+        n = len(mat)
+        
+        # Function to rotate matrix 90° clockwise
+        def rotate(matrix):
+            # Transpose
+            for i in range(n):
+                for j in range(i, n):
+                    matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+            
+            # Reverse each row
+            for i in range(n):
+                matrix[i].reverse()
+        
+        # Try all 4 rotations
+        for _ in range(4):
+            if mat == target:
+                return True
+            rotate(mat)
+        
+        return False
