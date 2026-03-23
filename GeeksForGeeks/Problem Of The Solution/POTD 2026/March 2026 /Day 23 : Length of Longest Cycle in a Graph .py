4ab11d7362edecfa807a11f836d8f895ee9262f3
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-03-23 19:42:42 +0530

    Update Day 23 : Length of Longest Cycle in a Graph .py

--- GeeksForGeeks/Problem Of The Solution/POTD 2026/March 2026 /Day 23 : Length of Longest Cycle in a Graph .py
+++ GeeksForGeeks/Problem Of The Solution/POTD 2026/March 2026 /Day 23 : Length of Longest Cycle in a Graph .py
@@ -1,1 +1,35 @@
+class Solution:
+    def longestCycle(self, V, edges):
+        # Create outgoing edge mapping
+        graph = [-1] * V
+        for u, v in edges:
+            graph[u] = v
+        
+        visited = [False] * V
+        max_cycle = -1
+        
+        for i in range(V):
+            if visited[i]:
+                continue
+            
+            curr = i
+            time = {}
+            step = 0
+            
+            while curr != -1 and not visited[curr]:
+                visited[curr] = True
+                time[curr] = step
+                step += 1
+                
+                next_node = graph[curr]
+                
+                if next_node in time:
+                    cycle_length = step - time[next_node]
+                    max_cycle = max(max_cycle, cycle_length)
+                    break
+                
+                curr = next_node
+        
+        return max_cycle
+        
 
