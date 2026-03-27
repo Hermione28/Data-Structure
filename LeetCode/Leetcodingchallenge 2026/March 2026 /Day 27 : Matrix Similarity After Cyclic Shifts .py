d0954249e4b695c78b88c1cd5e594246d464264b
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-03-27 20:58:53 +0530

    Update Day 27 : Matrix Similarity After Cyclic Shifts .py

--- LeetCode/Leetcodingchallenge 2026/March 2026 /Day 27 : Matrix Similarity After Cyclic Shifts .py
+++ LeetCode/Leetcodingchallenge 2026/March 2026 /Day 27 : Matrix Similarity After Cyclic Shifts .py
@@ -1,1 +1,16 @@
-
+class Solution(object):
+    def areSimilar(self, mat, k):
+        m = len(mat)
+        n = len(mat[0])
+        
+        shift = k % n
+        
+        for i in range(m):
+            for j in range(n):
+                if i % 2 == 0:  # even row -> left shift
+                    if mat[i][j] != mat[i][(j + shift) % n]:
+                        return False
+                else:  # odd row -> right shift
+                    if mat[i][j] != mat[i][(j - shift) % n]:
+                        return False
+        return True
