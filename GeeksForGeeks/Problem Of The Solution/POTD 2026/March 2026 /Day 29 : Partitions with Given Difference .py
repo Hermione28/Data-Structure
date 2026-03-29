292dd10aae6a106baceb6386914efa80277ea159
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-03-29 13:58:56 +0530

    Add countPartitions method to Solution class
    Implemented a solution to count partitions with a given difference using dynamic programming.

--- GeeksForGeeks/Problem Of The Solution/POTD 2026/March 2026 /Day 29 : Partitions with Given Difference .py
+++ GeeksForGeeks/Problem Of The Solution/POTD 2026/March 2026 /Day 29 : Partitions with Given Difference .py
@@ -1,1 +1,21 @@
+class Solution:
+    def countPartitions(self, arr, diff):
+        totalSum = sum(arr)
+        
+        # Edge case
+        if (totalSum + diff) % 2 != 0:
+            return 0
+        
+        target = (totalSum + diff) // 2
+        
+        # DP array
+        dp = [0] * (target + 1)
+        dp[0] = 1  # One way to make sum 0
+        
+        for num in arr:
+            # Traverse backwards
+            for j in range(target, num - 1, -1):
+                dp[j] += dp[j - num]
+        
+        return dp[target]
 
