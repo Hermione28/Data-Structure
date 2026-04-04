ca0969d44e0e555da881d32d56757ddabde35f20
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-04-04 11:26:39 +0530

    Update Day 4 : Decode the Slanted Ciphertext .py

--- LeetCode/Leetcodingchallenge 2026/April 2026 /Day 4 : Decode the Slanted Ciphertext .py
+++ LeetCode/Leetcodingchallenge 2026/April 2026 /Day 4 : Decode the Slanted Ciphertext .py
@@ -1,1 +1,28 @@
-
+class Solution(object):
+    def decodeCiphertext(self, encodedText, rows):
+        if rows == 1:
+            return encodedText
+        
+        n = len(encodedText)
+        cols = n // rows
+        
+        
+        matrix = []
+        idx = 0
+        for r in range(rows):
+            matrix.append(list(encodedText[idx:idx+cols]))
+            idx += cols
+        
+        
+        result = []
+        
+        for start_col in range(cols):
+            r, c = 0, start_col
+            
+            while r < rows and c < cols:
+                result.append(matrix[r][c])
+                r += 1
+                c += 1
+        
+        
+        return "".join(result).rstrip()
