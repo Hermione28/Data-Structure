603cfba0e1f970528c97fcb3d27ea1fed8f4cb33
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-04-07 15:15:44 +0530

    Update Day 7 :   Walking Robot Simulation II .py

--- LeetCode/Leetcodingchallenge 2026/April 2026 /Day 7 :   Walking Robot Simulation II .py
+++ LeetCode/Leetcodingchallenge 2026/April 2026 /Day 7 :   Walking Robot Simulation II .py
@@ -1,1 +1,51 @@
+class Robot(object):
 
+    def __init__(self, width, height):
+        self.path = []
+        
+        # bottom row (left → right)
+        for i in range(width):
+            self.path.append((i, 0))
+        
+        # right column (bottom → top)
+        for i in range(1, height):
+            self.path.append((width - 1, i))
+        
+        # top row (right → left)
+        for i in range(width - 2, -1, -1):
+            self.path.append((i, height - 1))
+        
+        # left column (top → bottom)
+        for i in range(height - 2, 0, -1):
+            self.path.append((0, i))
+        
+        self.perimeter = len(self.path)
+        self.idx = 0
+        self.moved = False
+
+    def step(self, num):
+        self.moved = True
+        self.idx = (self.idx + num) % self.perimeter
+
+    def getPos(self):
+        return list(self.path[self.idx])
+
+    def getDir(self):
+        if not self.moved:
+            return "East"
+        
+        # Special case
+        if self.idx == 0:
+            return "South"
+        
+        x1, y1 = self.path[self.idx - 1]
+        x2, y2 = self.path[self.idx]
+        
+        if x2 > x1:
+            return "East"
+        elif x2 < x1:
+            return "West"
+        elif y2 > y1:
+            return "North"
+        else:
+            return "South"
