f3ed832e5ab88a4925f3072cc715165fb2d7391a
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-04-08 09:55:19 +0530

    Update Day 8 : Segregate 0s and 1s .py

--- GeeksForGeeks/Problem Of The Solution/POTD 2026/April 2026 /Day 8 : Segregate 0s and 1s .py
+++ GeeksForGeeks/Problem Of The Solution/POTD 2026/April 2026 /Day 8 : Segregate 0s and 1s .py
@@ -1,1 +1,11 @@
-
+class Solution:
+    def segregate0and1(self, arr):
+        left = 0
+        right = len(arr) - 1
+        
+        while left < right:
+            if arr[left] == 0:
+                left += 1
+            else:
+                arr[left], arr[right] = arr[right], arr[left]
+                right -= 1
