1291ec96750dc44d3ec91248008baa26a799f15f
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-04-11 15:48:16 +0530

    Update Day 11 : Count increasing Subarrays .py

--- GeeksForGeeks/Problem Of The Solution/POTD 2026/April 2026 /Day 11 : Count increasing Subarrays .py
+++ GeeksForGeeks/Problem Of The Solution/POTD 2026/April 2026 /Day 11 : Count increasing Subarrays .py
@@ -1,1 +1,14 @@
+class Solution:
+    def countIncreasing(self, arr):
+        n = len(arr)
+        count = 0
+        length = 1  # current increasing streak length
 
+        for i in range(1, n):
+            if arr[i] > arr[i - 1]:
+                length += 1
+                count += (length - 1)
+            else:
+                length = 1
+
+        return count
