d93d6d71ec2489e18d1a855de4f19c8f50c5db5c
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-04-11 15:49:15 +0530

    Update Day 11 : Minimum Distance Between Three Equal Elements II .py

--- LeetCode/Leetcodingchallenge 2026/April 2026 /Day 11 : Minimum Distance Between Three Equal Elements II .py
+++ LeetCode/Leetcodingchallenge 2026/April 2026 /Day 11 : Minimum Distance Between Three Equal Elements II .py
@@ -1,1 +1,26 @@
-
+class Solution(object):
+    def minimumDistance(self, nums):
+        from collections import defaultdict
+        
+        index_map = defaultdict(list)
+        
+        
+        for i, num in enumerate(nums):
+            index_map[num].append(i)
+        
+        ans = float('inf')
+        
+       
+        for indices in index_map.values():
+            if len(indices) < 3:
+                continue
+            
+            
+            for i in range(len(indices) - 2):
+                p = indices[i]
+                r = indices[i + 2]
+                
+                distance = 2 * (r - p)
+                ans = min(ans, distance)
+        
+        return ans if ans != float('inf') else -1
