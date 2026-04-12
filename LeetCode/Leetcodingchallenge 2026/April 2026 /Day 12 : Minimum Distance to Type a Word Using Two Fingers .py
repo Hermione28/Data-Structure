d66ec5d4c57bad8642f1694b84dad6a6a587d40d
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-04-12 17:55:42 +0530

    Update Day 12 : Minimum Distance to Type a Word Using Two Fingers .py

--- LeetCode/Leetcodingchallenge 2026/April 2026 /Day 12 : Minimum Distance to Type a Word Using Two Fingers .py
+++ LeetCode/Leetcodingchallenge 2026/April 2026 /Day 12 : Minimum Distance to Type a Word Using Two Fingers .py
@@ -1,1 +1,28 @@
-
+class Solution(object):
+    def minimumDistance(self, word):
+        def dist(a, b):
+            if a is None:
+                return 0
+            x1, y1 = divmod(a, 6)
+            x2, y2 = divmod(b, 6)
+            return abs(x1 - x2) + abs(y1 - y2)
+        
+        n = len(word)
+        dp = [0] * 26  # dp[j] = max saving when second finger at j
+        res = 0
+        
+        for i in range(1, n):
+            cur = ord(word[i]) - ord('A')
+            prev = ord(word[i-1]) - ord('A')
+            
+            d = dist(prev, cur)
+            res += d
+            
+            new_dp = dp[:]
+            for j in range(26):
+                
+                new_dp[prev] = max(new_dp[prev], dp[j] + d - dist(j, cur))
+            
+            dp = new_dp
+        
+        return res - max(dp)
