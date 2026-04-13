ae9630e42e4d16d44317953f3f7ad1e4a6b8be7f
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-04-13 12:39:20 +0530

    Update Day 13 : Minimum Distance to the Target Element .py

--- LeetCode/Leetcodingchallenge 2026/April 2026 /Day 13 : Minimum Distance to the Target Element .py
+++ LeetCode/Leetcodingchallenge 2026/April 2026 /Day 13 : Minimum Distance to the Target Element .py
@@ -1,1 +1,9 @@
-
+class Solution(object):
+    def getMinDistance(self, nums, target, start):
+        ans = float('inf')
+        
+        for i in range(len(nums)):
+            if nums[i] == target:
+                ans = min(ans, abs(i - start))
+        
+        return ans
