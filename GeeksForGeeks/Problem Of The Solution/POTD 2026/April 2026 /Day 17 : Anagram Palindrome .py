d9ec34fdae11c3c8aaa97d9b0df5c06eb8bd4431
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-04-17 21:53:41 +0530

    Update Day 17 : Anagram Palindrome .py

--- GeeksForGeeks/Problem Of The Solution/POTD 2026/April 2026 /Day 17 : Anagram Palindrome .py
+++ GeeksForGeeks/Problem Of The Solution/POTD 2026/April 2026 /Day 17 : Anagram Palindrome .py
@@ -1,1 +1,16 @@
-
+class Solution:
+    def canFormPalindrome(self, s):
+        freq = {}
+        
+        # Count frequency
+        for ch in s:
+            freq[ch] = freq.get(ch, 0) + 1
+        
+        # Count odd frequencies
+        odd_count = 0
+        for count in freq.values():
+            if count % 2 != 0:
+                odd_count += 1
+        
+        # Check condition
+        return odd_count <= 1
