3462dc38499aeaf20a8ec146fb8261de36c121f0
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-04-18 21:54:39 +0530

    Update Day 18 : Flip to Maximize 1s .py

--- GeeksForGeeks/Problem Of The Solution/POTD 2026/April 2026 /Day 18 : Flip to Maximize 1s .py
+++ GeeksForGeeks/Problem Of The Solution/POTD 2026/April 2026 /Day 18 : Flip to Maximize 1s .py
@@ -1,1 +1,15 @@
-
+class Solution:
+    def maxOnes(self, arr):
+        total_ones = sum(arr)
+        
+        max_gain = 0
+        current_gain = 0
+        
+        for num in arr:
+            value = 1 if num == 0 else -1
+            
+            current_gain = max(value, current_gain + value)
+            max_gain = max(max_gain, current_gain)
+        
+        return total_ones + max_gain
+        
