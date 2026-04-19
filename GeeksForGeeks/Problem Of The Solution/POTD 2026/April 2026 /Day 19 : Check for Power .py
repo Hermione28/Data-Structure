fcb5aef545212056e5a85f7bf23cf8639006c913
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-04-19 22:25:25 +0530

    Update Day 19 : Check for Power .py

--- GeeksForGeeks/Problem Of The Solution/POTD 2026/April 2026 /Day 19 : Check for Power .py
+++ GeeksForGeeks/Problem Of The Solution/POTD 2026/April 2026 /Day 19 : Check for Power .py
@@ -1,1 +1,12 @@
+class Solution:
+    def isPower(self, x, y):
+      
+        if x == 1:
+            return y == 1
+        
+        
+        while y % x == 0:
+            y //= x
+        
+        return y == 1
 
