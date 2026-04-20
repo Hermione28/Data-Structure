fccc4d5e78a9d582067902baba916afb87f8b2eb
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-04-20 21:56:22 +0530

    Add maxDistance method to Solution class
    Implement maxDistance method to find the maximum distance between two houses of different colors.

--- LeetCode/Leetcodingchallenge 2026/April 2026 /Day 20 :  Two Furthest Houses With Different Colors .py
+++ LeetCode/Leetcodingchallenge 2026/April 2026 /Day 20 :  Two Furthest Houses With Different Colors .py
@@ -1,1 +1,19 @@
-
+class Solution(object):
+    def maxDistance(self, colors):
+        n = len(colors)
+        
+        max_dist = 0
+        
+        
+        for i in range(n - 1, -1, -1):
+            if colors[i] != colors[0]:
+                max_dist = max(max_dist, i - 0)
+                break
+        
+       
+        for i in range(n):
+            if colors[i] != colors[n - 1]:
+                max_dist = max(max_dist, (n - 1) - i)
+                break
+        
+        return max_dist
