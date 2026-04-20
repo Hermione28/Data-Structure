04178e4a29faf1bb9b891c3e99db7543357c3e3f
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-04-20 20:22:27 +0530

    Update Check if the Number is Armstrong .py

--- Striver A2Z Sheet /Basic Maths Problem/Check if the Number is Armstrong .py
+++ Striver A2Z Sheet /Basic Maths Problem/Check if the Number is Armstrong .py
@@ -1,1 +1,23 @@
-
+#An Amrstrong number is a number that is equal to the sum of its own digits each raised to the power of the number of digits.
+'''Calculate the number of digits in the input number and store it in k.
+Initialise a variable sum to 0. This variable will store the sum of each digit raised to the power of number of digits in number.
+Make a copy of the original number to store it in a temporary variable.
+Run a while loop with the condition n>0 and at each iteration:
+Get the last digit of n by using the modulus operator % with 10 and store it in a temporary variable ld.
+Add the digit ld raised to the power of k of the sum.
+Update n by integer division with 10 effectively removing the last digit.
+After the loop, check if the original input number is equal to the sum of the digits raised to the power of the number of digits in the number.
+If they are equal, return true indicating the number is an Armstrong number.
+If they are not equal, return false indicating that the number is not an Armstrong number.'''
+class Solution:
+    def isArmstrong(self, n: int) -> bool:
+        k = len(str(n))  # number of digits
+        sum = 0
+        num = n
+        
+        while num > 0:
+            ld = num % 10
+            sum = ld ** k + sum 
+            num //= 10
+        
+        return sum == n
