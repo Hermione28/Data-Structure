0e23682854fcabecd9c35094bf17b6f9cd8c395e
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-04-20 20:06:58 +0530

    Update Reverse a Number .py

--- Striver A2Z Sheet /Basic Maths Problem/Reverse a Number .py
+++ Striver A2Z Sheet /Basic Maths Problem/Reverse a Number .py
@@ -2,7 +2,9 @@ class Solution:
     def reverseNumber(self, n):
         revNum = 0
         while n > 0 :
+            #extract last digit from number 
             lastdigit = n % 10
             revNum = revNum * 10 + lastdigit
+            # remove the last digit from number
             n = n // 10
         return revNum
