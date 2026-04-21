33ae9195d693f3233beba007aeef0f3fe6c94bfd
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-04-21 21:50:28 +0530

    Update Quick Sort .py

--- Striver A2Z Sheet /Sorting/Quick Sort .py
+++ Striver A2Z Sheet /Sorting/Quick Sort .py
@@ -1,3 +1,13 @@
+'''Algorithm
+Merge Sort is a classic divide and conquer algorithm. The core idea is based on breaking down a big problem into smaller, manageable sub-problems i.e. sorting smaller arrays and then merging those solutions to get the final sorted result.
+
+It is much easier to merge two sorted arrays than to sort a large unsorted one. Therefore, instead of trying to sort the entire array at once, Merge Sort breaks it into halves repeatedly until we reach arrays of size 1 (which are trivially sorted), and then merges them back in sorted order. This makes the algorithm efficient and predictable, especially for large datasets.
+If the array has only one or zero elements, it is already sorted, so we return it as is.
+Else, we divide the array into two halves by finding the middle index.
+We then apply the merge sort algorithm recursively on each of the two halves to sort them individually.
+Once we have two sorted halves, we need to merge them into a single sorted array.
+To merge, we compare elements from both halves one by one and place the smaller element into a new array, continuing this until all elements from both halves are used.
+This process is repeated at every level of recursion, and finally, we get one fully sorted array after all merges are complete.'''
 
 class solution:
   def merge(self,nums,low,mid,high):
