cbe77c96beef1e4a393d19d507d8e564e60f2ff9
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-04-21 17:12:48 +0530

    Update Day 21 : Two water Jug problem .py

--- GeeksForGeeks/Problem Of The Solution/POTD 2026/April 2026 /Day 21 : Two water Jug problem .py
+++ GeeksForGeeks/Problem Of The Solution/POTD 2026/April 2026 /Day 21 : Two water Jug problem .py
@@ -1,1 +1,39 @@
+import math
 
+class Solution:
+    def pour(self, fromJug, toJug, d):
+        fromCap = fromJug
+        toCap = toJug
+        
+        fromJug = fromCap
+        toJug = 0
+        
+        step = 1
+        
+        while fromJug != d and toJug != d:
+            temp = min(fromJug, toCap - toJug)
+            toJug += temp
+            fromJug -= temp
+            step += 1
+            
+            if fromJug == d or toJug == d:
+                break
+            
+            if fromJug == 0:
+                fromJug = fromCap
+                step += 1
+            
+            if toJug == toCap:
+                toJug = 0
+                step += 1
+        
+        return step
+
+    def minSteps(self, m, n, d):
+        if d > max(m, n):
+            return -1
+        
+        if d % math.gcd(m, n) != 0:
+            return -1
+        
+        return min(self.pour(m, n, d), self.pour(n, m, d))
