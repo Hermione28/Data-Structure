5fe881cf90aceb9dcba7ff20983f13050a6defd3
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-04-22 15:19:28 +0530

    Update Day 22 : Mean of range in array .py

--- GeeksForGeeks/Problem Of The Solution/POTD 2026/April 2026 /Day 22 : Mean of range in array .py
+++ GeeksForGeeks/Problem Of The Solution/POTD 2026/April 2026 /Day 22 : Mean of range in array .py
@@ -1,1 +1,18 @@
-
+class Solution:
+    def findMean(self, arr, queries):
+        n = len(arr)
+        
+        
+        prefix = [0] * (n + 1)
+        for i in range(n):
+            prefix[i + 1] = prefix[i] + arr[i]
+        
+        
+        result = []
+        for l, r in queries:
+            total = prefix[r + 1] - prefix[l]
+            length = r - l + 1
+            mean = total // length
+            result.append(mean)
+        
+        return result
