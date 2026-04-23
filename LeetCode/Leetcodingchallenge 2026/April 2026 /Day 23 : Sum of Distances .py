4d332ae60f5ccfee1caa9f4f95426d1c280e4708
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-04-23 13:55:48 +0530

    Update Day 23 : Sum of Distances .py

--- LeetCode/Leetcodingchallenge 2026/April 2026 /Day 23 : Sum of Distances .py
+++ LeetCode/Leetcodingchallenge 2026/April 2026 /Day 23 : Sum of Distances .py
@@ -1,1 +1,33 @@
+class Solution(object):
+    def distance(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
 
+        n = len(nums)
+        res = [0] * n
+        groups = defaultdict(list)
+        
+        for i in range(n):
+            groups[nums[i]].append(i)
+        
+        for indices in groups.values():
+            prefix = 0
+            total = sum(indices)
+            size = len(indices)
+            
+            for i in range(size):
+                idx = indices[i]
+                
+                left = idx * i - prefix
+                right = (total - prefix - idx) - idx * (size - i - 1)
+                
+                res[idx] = left + right
+                
+                prefix += idx
+        
+        return res
+
+        
