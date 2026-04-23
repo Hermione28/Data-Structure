3f6051836a3b91020b27a50ee01555b87a9a7213
Hermione28 <115934537+Hermione28@users.noreply.github.com>
2026-04-23 13:48:44 +0530

    Add twoEditWords method to Solution class
    Implement a solution to find words within two edits of a dictionary.

--- LeetCode/Leetcodingchallenge 2026/April 2026 /Day 22 : Words Within Two Edits of Dictionary .py
+++ LeetCode/Leetcodingchallenge 2026/April 2026 /Day 22 : Words Within Two Edits of Dictionary .py
@@ -1,1 +1,20 @@
-
+class Solution(object):
+    def twoEditWords(self, queries, dictionary):
+        result = []
+        
+        for q in queries:
+            for d in dictionary:
+                diff = 0
+                
+                # count differences
+                for i in range(len(q)):
+                    if q[i] != d[i]:
+                        diff += 1
+                    if diff > 2:
+                        break
+                
+                if diff <= 2:
+                    result.append(q)
+                    break   # no need to check other dictionary words
+        
+        return result
